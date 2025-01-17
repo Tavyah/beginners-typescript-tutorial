6502aaecdc57c223333517fde7a518551f146aa6
Tavyah <tavylizia@gmail.com>
2025-01-17 09:22:31 +0100

    completed task 13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -4,7 +4,7 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
       throw new Error("Failure!");
-    }
+    } // Catch som any for å få melding ut ellers unknown og da får man ikke hentet ut meldingen. Kan åsså parse den til en Error me bruk av e as Error.message
   } catch (e: any) {
     return e.message;
   }
